8e2afedf924574f1f0ee08205d86a1724a210036
turim0 <ivan_medvedev_1996@mil.ru>
2019-01-07 19:25:22 +0300

    исправлено

--- bars.py
+++ bars.py
@@ -22,24 +22,27 @@ def get_smallest_bar(data):
 def get_closest_bar(data, my_longitude, my_latitude):
     my_latitude *= math.pi / 180
     my_longitude *= math.pi / 180
+
     def calculation(longitude, latitude):
-        R = 6371000
+        earth_radius = 6371000
         latitude *= math.pi / 180
         longitude *= math.pi / 180
-        d = math.acos(math.sin(my_latitude)*math.sin(latitude)+
+        distance = math.acos(math.sin(my_latitude)*math.sin(latitude) +
                       math.cos(my_latitude)*math.cos(latitude)*math.cos(my_longitude-longitude))
-        L = d*R
-        return L
-    closest_bar = min(data['features'], key = lambda index: calculation(index['geometry']['coordinates'][0],
-                                                                        index['geometry']['coordinates'][1]) )
+        return distance*earth_radius
+    closest_bar = min(data['features'], key=lambda index: calculation(
+        index['geometry']['coordinates'][0],
+        index['geometry']['coordinates'][1])
+                      )
     return closest_bar['properties']['Attributes']['Name']
+
 
 if __name__ == '__main__':
     print("Введите путь до файла: ")
     file_path = input()
     data = load_data(file_path)
-    print('Самый большой бар:',get_biggest_bar(data))
-    print('Самый маленький бар:',get_smallest_bar(data))
+    print('Самый большой бар:', get_biggest_bar(data))
+    print('Самый маленький бар:', get_smallest_bar(data))
     latitude = float(input("Введите широту вашего текущего местоположения:"))
     longitude = float(input("Введите долготу вашего текущего местоположения:"))
     print('Ближайший бар:', get_closest_bar(data, longitude, latitude))
